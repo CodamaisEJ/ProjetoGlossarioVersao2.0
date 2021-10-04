6ebf1018a18c3881d19f8374acaea134b197dabd
DouglasFeliphe <douglasfeliphe@hotmail.com>
2021-10-04 11:07:25 -0300

    instalação e configuração do axios

--- frontend-react/src/pages/Login/index.js
+++ frontend-react/src/pages/Login/index.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Header from "../../components/Header";
+import api from "../../services/api";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLogin() {
+    alert(username + " " + password);
+    try {
+      const response = await api.get("/");
+    } catch (error) {}
+  }
+
   return (
     <div>
       <Header />
@@ -14,14 +25,24 @@ export default function Login() {
             <div class="login-input">
               <label for="user">Usuário:</label>
               <br />
-              <input type="text" id="user" placeholder="Usuário" />
+              <input
+                type="text"
+                id="user"
+                placeholder="Usuário"
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
             <div class="login-input">
               <label for="password">Senha: </label>
               <br />
-              <input type="password" id="password" placeholder="********" />
+              <input
+                type="password"
+                id="password"
+                placeholder="********"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-            <button id="acesso" onclick="validador()">
+            <button id="acesso" onClick={handleLogin}>
               <i class="fas fa-user-friends"></i>Acessar
             </button>
           </div>
