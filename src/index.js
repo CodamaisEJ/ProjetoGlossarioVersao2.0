e10b1ebc14553ee5bec6ae35e11d4b5377ddaea9
Codamais <73510665+CodamaisEJ@users.noreply.github.com>
2021-10-09 15:01:26 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const app = express()
-const port = process.env.PORT || 3000;
+//const port = 3000
 
 const routes = require('./routes')
 
 routes(app)
 
-app.listen(port, () => {
-    console.log(`Servidor em execução na porta ${port}`)
-})
+//app.listen(port, () => {
+   // console.log(`Servidor em execução na porta ${port}`)
+//})
+
+app.listen(process.env.PORT || 3000)
 
 module.exports = app
