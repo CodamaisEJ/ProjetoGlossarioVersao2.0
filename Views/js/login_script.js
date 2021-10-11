4ac4b7acc163ab01650495ae6f3465d90d82f9f3
MarcusVSRDev <marcusvinicius616@gmail.com>
2021-10-11 14:18:06 -0300

    Ajustando o footer e o header

--- Views/js/login_script.js
+++ Views/js/login_script.js
@@ -3,16 +3,19 @@ function mudaCor(){
   console.log(x)
   if(x == 1){
     var backgroundColor = "#314C85";
+    document.getElementById("header").style.backgroundColor = backgroundColor;
     document.getElementById("acesso").style.backgroundColor = backgroundColor;
     document.getElementById("footer").style.backgroundColor = backgroundColor;
   }
   if(x == 2){
     var backgroundColor = "#0D4F3F";
+    document.getElementById("header").style.backgroundColor = backgroundColor;
     document.getElementById("acesso").style.backgroundColor = backgroundColor;
     document.getElementById("footer").style.backgroundColor = backgroundColor;
   }
   if(x == 3){
     var backgroundColor = "#430D64";
+    document.getElementById("header").style.backgroundColor = backgroundColor;
     document.getElementById("acesso").style.backgroundColor = backgroundColor;
     document.getElementById("footer").style.backgroundColor = backgroundColor;
   }
