8e9f5d5b6f230e8b326194f35d3ec37d7c540ddc
Leonardo Lopes <codamais.ej@gmail.com>
2021-10-22 11:04:36 -0300

    Modal instalado
    Funcionado modal com bugs

--- Views/js/menu_dropdown.js
+++ Views/js/menu_dropdown.js
@@ -1,28 +1,29 @@
 function myFunction() {
-        document.getElementById("links").style.marginRight = "7rem"
-        document.getElementById("dropdown").style.position = "absolute";
-        document.getElementById("dropdown").style.right = "0px";
-        document.getElementById("dropdown").style.marginTop = "5rem";
-        document.getElementById("dropdown").style.marginRight = "3.5rem";
-        document.getElementById("logo_notificacao").style.display = "none";
-        document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+  document.getElementById("dropdown").style.right = "0px";
+  document.getElementById("dropdown").style.marginRight = "3.5rem";
+  document.getElementById("logo_notificacao").style.display = "none";
+  document.getElementById("myDropdown").classList.toggle("show");
+  var Switch = document.getElementById("dropbtn");
+  var toggled = document.getElementById("myDropdown");
+}
+var x = localStorage.getItem("tipoUsuario");
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-          document.getElementById("links").style.marginRight = "-27rem"
-          document.getElementById("dropdown").style.position = "relative";
-          document.getElementById("dropdown").style.right = "0px";
-          document.getElementById("dropdown").style.marginTop = "0rem";
-          document.getElementById("dropdown").style.marginRight = "1rem";
-          document.getElementById("logo_notificacao").style.display = "flex";
-        }
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+              document.getElementById("dropdown").style.position = "relative";
+              document.getElementById("dropdown").style.right = "0px";
+              document.getElementById("dropdown").style.marginTop = "0rem";
+              document.getElementById("dropdown").style.marginRight = "3.5rem";
+              if (x == 3) {
+                  document.getElementById("logo_notificacao").style.display = "none";
+              } else {
+                  document.getElementById("logo_notificacao").style.display = "flex";
+              }
+          }
       }
-    }
   }
+}
