83891c38c797f0a6827b9c5bc4e6a1cfa9e606ae
Leonardo Lopes <codamais.ej@gmail.com>
2021-12-11 10:19:30 -0300

    revert

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -1,53 +1,56 @@
-const database = require("../models");
-const jwt = require("jsonwebtoken");
-const authConfig = require("../config/auth");
-const bcrypt = require("bcrypt");
+const database = require('../models')
+const jwt = require('jsonwebtoken')
+const authConfig = require('../config/auth')
+
 class TokenController {
-  static async createToken(req, res, next) {
-    const { email, senha } = req.body;
-    try {
-      const userEmail = await database.Users.findOne({ where: { email } });
-      let hashedPassword = "";
 
-      if (userEmail) {
-        hashedPassword = userEmail.senha;
-      }
+    static async createToken(req, res, next){
+        const { email, senha } = req.body
+        try { 
 
-      if (!userEmail) {
-        return res.status(401).json({ error: "E-mail não encontrado!" });
-      }
+            const userEmail = await database.Users.findOne({ where: { email } })
 
-      //   desencripta a senha
-      let isPasswordValid = await bcrypt.compare(senha, hashedPassword);
+            if(!userEmail){
+                return res.status(401).json({error: "E-mail não encontrado!"})
+            }
 
-      const userSenha = await database.Users.findOne({ where: { senha } });
+            const userSenha = await database.Users.findOne({ where: { senha } })
 
-      if (!userSenha && !isPasswordValid) {
-        return res.status(401).json({ error: "Senha inválida!" });
-      }
+            if(!userSenha){
+                return res.status(401).json({error: "Senha não encontrada!"})
+            }
 
-      const { id, nome, tipo_de_usuario } = userSenha;
+            const { id, nome, tipo_de_usuario } = userSenha
 
-      return await res.json({
-        user: { id, nome, email, tipo_de_usuario },
-        token: jwt.sign({ id }, authConfig.secret, {
-          expiresIn: authConfig.expiresIn,
-        }),
-      });
-    } catch (error) {
-      return res.status(500).json(error.message);
+            return await res.json({ 
+                user: { id, nome, email, tipo_de_usuario },
+                token: jwt.sign({ id }, authConfig.secret, {
+                    expiresIn: authConfig.expiresIn
+                }
+                )
+            })
+
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
     }
-  }
+    
+    static async destroyToken(req, res, next){
+        const token = req.body
+        try { 
 
-  static async destroyToken(req, res, next) {
-    const token = req.body;
-    try {
-      await jwt.destroy(token);
-      return res.redirect("/");
-    } catch (error) {
-      return res.status(500).json(error.message);
+            await jwt.destroy(token)
+            return res.redirect('/')
+
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
     }
-  }
+
 }
 
+<<<<<<< HEAD
 module.exports = TokenController;
+=======
+module.exports = TokenController
+>>>>>>> parent of 47cc3f2... criptografia de senha
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,130 +1,104 @@
-const database = require("../models");
-const bcrypt = require("bcrypt");
+const database = require('../models')
+
 class UserController {
-  static async createUser(req, res) {
-    // const newUser = req.body
-    let newUser = {};
-    // pegar senha do body
-    const { nome, email, senha, tipo_de_usuario } = req.body;
-    console.log("req.body: ", req.body);
-    // criptografar senha
-    try {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(senha, salt);
-      newUser = {
-        nome,
-        email,
-        senha: hashedPassword,
-        tipo_de_usuario,
-      };
-    } catch (error) {
-      return res
-        .status(500)
-        .json("Erro ao criptografar senha: " + error.message);
-    }
 
-    // criar usuario
-    console.log("new user: ", newUser);
-
-    try {
-      const newUserCreate = await database.Users.create(newUser);
-      return res.status(201).json(newUserCreate);
-    } catch (error) {
-      return res.status(500).json("Erro ao criar usuario: " + error.message);
-    }
-  }
-
-  //funcao para usuarios admins
-  //Atualizar perfis de todos os usuarios
-  static async updateUser(req, res) {
-    const { id } = req.params;
-    const newData = req.body;
-    try {
-      if (isNaN(id)) {
-        return res.json({ error: "id não informado ou inválido!" });
+    static async createUser(req, res){
+      const newUser = req.body
+      try {
+        const newUserCreate = await database.Users.create(newUser)
+        return res.status(201).json(newUserCreate)
+      } catch (error) {
+          return res.status(500).json(error.message)
       }
-      await database.Users.update(newData, { where: { id: Number(id) } });
-      const updateUser = await database.Users.findOne({
-        where: { id: Number(id) },
-      });
-      return res.status(200).json(updateUser);
-    } catch (error) {
-      return res.status(500).json(error.message);
     }
-  }
+    
+    //funcao para usuarios admins
+    //Atualizar perfis de todos os usuarios
+    static async updateUser(req, res){
+      const { id } = req.params
+      const newData = req.body
+      try {
+        if(isNaN(id)){
+          return res.json({ error: "id não informado ou inválido!" })
+        }
+        await database.Users.update(newData, { where: { id: Number(id) }})
+        const updateUser = await database.Users.findOne({ where: { id: Number(id) }})
+        return res.status(200).json(updateUser)
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
 
-  //funcao para usuarios sem ser admin
-  //Atualizar seu proprio perfil
-  static async updateUserMydata(req, res) {
-    const { id } = req.params;
-    const newData = req.body;
-    const { passwordCurrentBody } = req.body;
-    try {
-      if (isNaN(id)) {
-        return res.json({ error: "id não informado ou inválido!" });
+    //funcao para usuarios sem ser admin
+    //Atualizar seu proprio perfil
+    static async updateUserMydata(req, res){
+      const { id } = req.params
+      const newData = req.body
+      const { passwordCurrentBody } = req.body
+      try {
+        if(isNaN(id)){
+          return res.json({ error: "id não informado ou inválido!" })
+        }
+        const findUser = await database.Users.findOne({ where: { id: Number(id) }})
+        const passwordCurrent = findUser.senha
+        if(passwordCurrentBody !== passwordCurrent){
+          return res.json({ message: 'Senha não identica ao do banco' })
+        }
+        await database.Users.update(newData, { where: { id: Number(id) }})
+        const updateUser = await database.Users.findOne({ where: { id: Number(id) }})
+        return res.status(200).json(updateUser)
+      } catch (error) {
+        return res.status(500).json(error.message)
       }
-      const findUser = await database.Users.findOne({
-        where: { id: Number(id) },
-      });
-      const passwordCurrent = findUser.senha;
-      if (passwordCurrentBody !== passwordCurrent) {
-        return res.json({ message: "Senha não identica ao do banco" });
-      }
-      await database.Users.update(newData, { where: { id: Number(id) } });
-      const updateUser = await database.Users.findOne({
-        where: { id: Number(id) },
-      });
-      return res.status(200).json(updateUser);
-    } catch (error) {
-      return res.status(500).json(error.message);
     }
-  }
 
-  static async deleteUser(req, res) {
-    const { id } = req.params;
-    try {
-      if (isNaN(id)) {
-        return res.json({ error: "id não informado ou inválido!" });
+    static async deleteUser(req, res){
+      const { id } = req.params
+      try {
+        if(isNaN(id)){
+          return res.json({ error: "id não informado ou inválido!" })
+        }
+        const deleteUser = await database.Users.destroy({ where: { id: Number(id) }})
+        if(!deleteUser){
+          return res.json({ error: "Usuário não encontrado pelo id informado!" })
+        }
+        return res.status(200).json({ message: `id ${id} foi deletado com sucesso!` })
+      } catch (error) {
+        return res.status(500).json(error.message)
       }
-      const deleteUser = await database.Users.destroy({
-        where: { id: Number(id) },
-      });
-      if (!deleteUser) {
-        return res.json({ error: "Usuário não encontrado pelo id informado!" });
-      }
-      return res
-        .status(200)
-        .json({ message: `id ${id} foi deletado com sucesso!` });
-    } catch (error) {
-      return res.status(500).json(error.message);
     }
-  }
+    
+    static async listUsers(req, res){
+      try {
+        const listUsers = await database.Users.findAll(
+          {
+            order: [
+              ['id', 'DESC']
+            ],
+          }
+        )
+        return res.status(200).json(listUsers)  
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
 
-  static async listUsers(req, res) {
-    try {
-      const listUsers = await database.Users.findAll({
-        order: [["id", "DESC"]],
-      });
-      return res.status(200).json(listUsers);
-    } catch (error) {
-      return res.status(500).json(error.message);
-    }
-  }
-
-  static async listOneUser(req, res) {
-    const { id } = req.params;
-    try {
-      if (isNaN(id)) {
-        return res.json({ error: "id não informado ou inválido!" });
+    static async listOneUser(req, res) {
+      const { id } = req.params
+      try {
+        if(isNaN(id)){
+          return res.json({ error: "id não informado ou inválido!" })
+        }
+        const oneUser = await database.Users.findOne( { where: { id: Number(id) }})
+        return res.status(200).json(oneUser)
+      } catch (error) {
+        return res.status(500).json(error.message)
       }
-      const oneUser = await database.Users.findOne({
-        where: { id: Number(id) },
-      });
-      return res.status(200).json(oneUser);
-    } catch (error) {
-      return res.status(500).json(error.message);
     }
-  }
 }
 
+<<<<<<< HEAD
 module.exports = UserController;
+=======
+module.exports = UserController
+>>>>>>> parent of 47cc3f2... criptografia de senha
